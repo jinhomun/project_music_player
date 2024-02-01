3b9a21ce61cf134c33a1832ad593834469d5fa7e
jinhomun <answlsgh95@gmail.com>
2024-02-01 18:30:54 +0900

    2024-02-01

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,40 +3,30 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '2157171533msh40b591d111aac24p1a7d8djsn01109726b8bd');
+      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;
-
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-// export const shazamCoreApi = createApi({
-//   reducerPath: 'shazamCoreApi',
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: 'https://shazam-core7.p.rapidapi.com',
-//     prepareHeaders: (headers) => {
-//       headers.set('X-RapidAPI-Key', '2157171533msh40b591d111aac24p1a7d8djsn01109726b8bd');
-
-//       return headers;
-//     },
-//   }),
-//   endpoints: (builder) => ({
-//     getTopCharts: builder.query({ query: () => '/charts/get-top-songs-in-world' }),
-//   }),
-// });
-
-// export const {
-//   useGetTopChartsQuery,
-// } = shazamCoreApi;
-
